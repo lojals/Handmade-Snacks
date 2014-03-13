04997a982a29d63851db0933b232e9f5a54a787d
Jorge Ovalle <jroz9105@gmail.com>
2014-03-12 21:26:12 -0500

    Initial Commit

---
+++ skipersTests/skipersTests.m
@@ -0,0 +1,34 @@
+//
+//  skipersTests.m
+//  skipersTests
+//
+//  Created by Jorge Raul Ovalle Zuleta on 3/12/14.
+//  Copyright (c) 2014 jomobile.co. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface skipersTests : XCTestCase
+
+@end
+
+@implementation skipersTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
